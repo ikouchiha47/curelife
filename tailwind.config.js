65ad6d1831eebae8ee53f60c1091813595563256
ikouchiha47 <princeofpersiaa3.ag@gmail.com>
2022-02-01 22:36:19 +0530

    feat: fixes package issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 module.exports = {
-  content: [
-    './app/views/**/*.html.erb',
-    './app/views/**/*.js.erb',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js'
-  ]
+    content: [
+        './app/views/**/*.html.erb',
+        './app/views/**/*.js.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js'
+    ],
+    plugins: [
+        require('postcss-import'),
+        require('postcss-flexbugs-fixes'),
+        require("tailwindcss"),
+        require('postcss-preset-env')({
+            autoprefixer: {
+                flexbox: 'no-2009'
+            },
+            stage: 3
+        })
+    ]
 }
